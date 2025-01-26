5059c8c14329a4e9dfa43221db1f87624abc3f73
Kenil Vora <kenilvora2509@gmail.com>
2025-01-26 12:15:36 +0530

    Initialize Frontend

---
+++ server/src/models/Dispute.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+interface IDispute extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId;
+  userId: mongoose.Schema.Types.ObjectId;
+  againstWhomId: mongoose.Schema.Types.ObjectId;
+  againstWhom: "User" | "Item";
+  reason: string;
+  images?: string[];
+  status: "Open" | "Resolved" | "Closed";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const disputeSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    againstWhomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "againstWhom",
+      required: true,
+    },
+    againstWhom: {
+      type: String,
+      required: true,
+      enum: ["User", "Item"],
+    },
+    reason: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    images: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+      enum: ["Open", "Resolved", "Closed"],
+      default: "Open",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IDispute>("Dispute", disputeSchema);
