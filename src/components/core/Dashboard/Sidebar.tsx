a160e6752dc41ec28e2f711ea65f9b9074e2a826
Kenil Vora <kenilvora2509@gmail.com>
2025-03-14 14:53:27 +0530

    Sideabar Animation Change

--- src/components/core/Dashboard/Sidebar.tsx
+++ src/components/core/Dashboard/Sidebar.tsx
@@ -23,20 +23,22 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) => {
     <div
       className={`bg-white px-3 max-[600px]:px-2 py-3 border-r border-neutral-200 flex flex-col gap-5 overflow-y-auto overflow-x-hidden
                 ${
-                  isSidebarOpen ? "min-w-[260px] " : "min-w-[60px]"
-                } transition-all duration-300 ease-in-out
+                  isSidebarOpen ? "min-w-[260px]" : "min-w-[60px]"
+                } transition-all duration-[800ms] ease-in-out
         `}
     >
-      <div className="text-xl font-bold flex justify-between items-center pl-0.5">
-        <span
-          className={` transition-all duration-300 ease-in-out
-            ${isSidebarOpen ? "flex" : "hidden"}
+      <div className="text-xl font-bold flex items-center pl-0.5 w-full relative">
+        <div
+          className={` transition-all duration-[800ms] ease-in-out absolute
+            ${isSidebarOpen ? "opacity-100 visible" : "opacity-0 invisible"}
           `}
+          id="sidebar-title"
         >
           Menu
-        </span>
+        </div>
         <div
-          className={`text-black hover:cursor-pointer hover:bg-neutral-200 rounded-sm p-1.5 relative`}
+          className={`text-black hover:cursor-pointer hover:bg-neutral-200 rounded-sm p-1.5 relative transition-all duration-[800ms] 
+            ease-in-out ml-auto`}
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
         >
           {isSidebarOpen ? (
@@ -54,13 +56,13 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) => {
             key={link.id}
             title={link.name}
             className={`flex items-center relative gap-4 text-neutral-600 hover:text-neutral-900 hover:cursor-pointer hover:bg-neutral-200 
-                      px-2 py-2 rounded-lg
+                      px-2 py-2 rounded-lg min-w-[40px]
                         ${currentPage?.id === link.id ? "bg-neutral-200" : ""}
                       `}
           >
             <link.icon size={24} />
             <span
-              className={`absolute left-10 whitespace-nowrap overflow-hidden transition-all duration-300 ease-in-out
+              className={`absolute left-10 whitespace-nowrap overflow-hidden transition-all duration-[800ms] ease-in-out
                           ${
                             isSidebarOpen
                               ? "opacity-100 visible"
@@ -82,7 +84,7 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) => {
         >
           <LuLogOut size={24} />
           <span
-            className={`absolute left-10 whitespace-nowrap overflow-hidden transition-all duration-300 ease-in-out
+            className={`absolute left-10 whitespace-nowrap overflow-hidden transition-all duration-[800ms] ease-in-out
                           ${
                             isSidebarOpen
                               ? "opacity-100 visible"
