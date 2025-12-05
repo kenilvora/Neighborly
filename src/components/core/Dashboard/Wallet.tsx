17901b7716af3ec22b04e3e871b5f60342ec0b04
kenilvora <kenilvora2509@gmail.com>
2025-12-05 23:25:12 +0530

    Domain settings updated

--- src/components/core/Dashboard/Wallet.tsx
+++ src/components/core/Dashboard/Wallet.tsx
@@ -27,7 +27,7 @@ const Wallet = () => {
 
   const submitHandler: SubmitHandler<WalletForm> = async (data) => {
     try {
-      const skt = new WebSocket("wss://neighborly-backend.kenilvora.tech");
+      const skt = new WebSocket("wss://neighborly-backend.kenilvora.dev");
 
       const startingMessage = {
         type: "payment_notification",
